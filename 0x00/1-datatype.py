08af36f92618f200c1f57d64e0289f15c0a911b3
etim-ipee <imaikopelijah001@gmail.com>
2026-04-10 13:58:57 +0100

    a file that contain datatype

---
+++ 0x00/1-datatype.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# a module that displays the datatype of a variable
+
+firstname = "caleb"
+lastname = "moses"
+salary = 1000000.00
+is_married = False
+age = 14
+
+print(type(firstname))
+print(type(lastname))
+print(type(salary))
+print(type(is_married))
+print(type(age))
