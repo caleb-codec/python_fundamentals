7ab062f8e0ae441212c508b95de3764cac972d65
etim-ipee <imaikopelijah001@gmail.com>
2026-04-10 14:05:05 +0100

    a file that contain format

---
+++ 0x00/4-format.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# a module that use string format 
+
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+age = input("Enter your age: ")
+salary = input("Enter your salary is: ")
+price = input("Enter your price: ")
+
+
+print(f"Welcome back, {firstname} {lastname}")
+print(f"you are {age} years old")
+print(f"your salary is {salary}")
+print(f"your price is {price}")
